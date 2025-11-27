b63e384a777234df0d83e81fe0017a3680fe3b75
TwistableDoor73 <jesusefrainbm29@gmail.com>
2025-11-27 01:51:38 -0600

    Changed db

--- Components/db.php
+++ Components/db.php
@@ -2,34 +2,18 @@
 function connectDatabase()
 {
     $host = 'localhost';
-    $user = 'root';
+    $user = 'u661724798_Admin';
     $password = 'FEDJnosotros4@-_-';
-    $dbname = 'RabaNet';
+    $dbname = 'u661724798_RabaNet';
     $port = 3306;
-    $socket = '/tmp/mysql.sock';
 
-    function checkUserType($requiredUserType = 2)
-    {
-        if (!isset($_SESSION['user_id'])) {
-            header('Location: login.php');
-            exit();
+    try {
+        $conn = new mysqli($host, $user, $password, $dbname, $port);
+        if ($conn->connect_error) {
+            throw new Exception("Connection failed: " . $conn->connect_error);
         }
-
-        // Depuración: Imprime los valores actuales
-        error_log("User ID: " . $_SESSION['user_id']);
-        error_log("User Type: " . $_SESSION['usr_type']);
-        error_log("Required Type: " . $requiredUserType);
-
-        if (!isset($_SESSION['usr_type']) || (int) $_SESSION['usr_type'] !== (int) $requiredUserType) {
-            echo "<p>No tienes permiso para acceder a esta página.</p>";
-            exit();
-        }
+    } catch (Exception $e) {
+        die("Connection error: " . $e->getMessage());
     }
-
-    $mysqli = new mysqli(hostname: $host, username: $user, password: $password, database: $dbname, port: $port, socket: $socket);
-
-    if ($mysqli->connect_errno) {
-        die("Connection error: " . $mysqli->connect_error);
-    }
-    return $mysqli;
+    return $conn;
 }
